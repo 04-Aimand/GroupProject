ec9b5026b9802ebc8b696182fba15e4b664ac35a
Xing Ye <tanxingye23@gmail.com>
2023-11-06 15:47:26 +0800

    Added Tower
    Prototype Tower

---
+++ Assets/Sprites/ShootingScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingScript : MonoBehaviour
+{
+    public AudioSource AS;
+    public AudioClip StartingUp, Shooting, Upgrade;
+    public Transform ShootingPoint;
+    public GameObject FireProjectile;
+    void Start()
+    {
+        AS.clip = StartingUp;
+        AS.Play();
+    }
+
+
+    public void Update()
+    {
+        if (InRadius)
+        {
+            Shoot();
+        }
+    }
+    private IEnumerator Shoot()
+    {
+        Instantiate(FireProjectile, ShootingPoint.transform.position, ShootingPoint.transform.rotation);
+        yield return new WaitForSeconds(3f);
+    }
+}
