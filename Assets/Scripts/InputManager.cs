6ad3af0e4bae69cc55608642eaa86ec97f8870e5
04-Aimand <heera.aimand@gmail.com>
2023-11-06 09:27:21 +0800

    Added Tower Placement Grid
    For players to check where they can place the towers

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class InputManager : MonoBehaviour
 {
@@ -12,12 +14,31 @@ public class InputManager : MonoBehaviour
     [SerializeField]
     private LayerMask placementLayermask;
 
+    public event Action OnClicked, OnExit;
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            OnClicked?.Invoke();
+        }
+        if(Input.GetMouseButtonDown(1))
+        {
+            OnExit?.Invoke();
+        }
+    }
+
+    public bool IsPointerOverUI()
+        => EventSystem.current.IsPointerOverGameObject(placementLayermask);
+    
+
     public Vector3 GetSelectedMapPosition()
     {
         Vector3 mousePos = Input.mousePosition;
         mousePos.z = sceneCamera.nearClipPlane;
         Ray ray = sceneCamera.ScreenPointToRay(mousePos);
         RaycastHit hit;
+
         if (Physics.Raycast(ray, out hit, 100, placementLayermask))
         {
             lastposition = hit.point;
--- Assets/Scripts/PlacementSystem.cs
+++ Assets/Scripts/PlacementSystem.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -11,10 +12,89 @@ public class PlacementSystem : MonoBehaviour
     [SerializeField]
     private Grid grid;
 
-    private void Update()
+    [SerializeField]
+    private TowerDatabaseSO database;
+    private int selectedTowerIndex;
+
+    [SerializeField]
+    private GameObject[] gridVisualization;
+
+    private GridData floorData, towerData;
+
+    private Renderer previewRenderer;
+
+    private List<GameObject> placedGameObjects = new List<GameObject>();
+    private void Start()
     {
+        StopPlacement();
+        floorData = new GridData();
+        towerData = new GridData();
+        previewRenderer = cellIndicator.GetComponentInChildren<Renderer>();
+    }
+
+    public void StartPlacement(int ID)
+    {
+        StopPlacement();
+        selectedTowerIndex = database.towersData.FindIndex(data => data.ID == ID);
+        if (selectedTowerIndex < 0)
+        {
+            Debug.LogError($"No ID Found {ID}");
+            return;
+        }
+        gridVisualization[0].SetActive(true);
+        cellIndicator.SetActive(true);
+        inputManager.OnClicked += PlaceTower;
+        inputManager.OnExit += StopPlacement;
+    }
+
+    private void PlaceTower()
+    {
+        if (inputManager.IsPointerOverUI())
+        {
+            return;
+        }
+
         Vector3 mousePosition = inputManager.GetSelectedMapPosition();
         Vector3Int gridposition = grid.WorldToCell(mousePosition);
+
+        bool placementValidity = CheckPlacementValidity(gridposition, selectedTowerIndex);
+        if (placementValidity == false)
+            return;
+
+        GameObject newTower = Instantiate(database.towersData[selectedTowerIndex].Prefab);
+        newTower.transform.position = grid.CellToWorld(gridposition);
+        placedGameObjects.Add(newTower);
+        GridData selectedData = database.towersData[selectedTowerIndex].ID == 0 ? floorData : towerData;
+        selectedData.AddObjectAt(gridposition, database.towersData[selectedTowerIndex].Size,
+            database.towersData[selectedTowerIndex].ID, placedGameObjects.Count - 1);
+    }
+
+    private bool CheckPlacementValidity(Vector3Int gridposition, int selectedTowerIndex)
+    {
+        GridData selectedData = database.towersData[selectedTowerIndex].ID == 0 ? floorData : towerData;
+
+        return selectedData.CanPlaceObjectAt(gridposition, database.towersData[selectedTowerIndex].Size);
+    }
+
+    private void StopPlacement()
+    {
+        selectedTowerIndex = -1;
+        gridVisualization[0].SetActive(false);
+        cellIndicator.SetActive(false);
+        inputManager.OnClicked -= PlaceTower;
+        inputManager.OnExit -= StopPlacement;
+    }
+
+    private void Update()
+    {
+        if (selectedTowerIndex < 0)
+            return;
+        Vector3 mousePosition = inputManager.GetSelectedMapPosition();
+        Vector3Int gridposition = grid.WorldToCell(mousePosition);
+
+        bool placementValidity = CheckPlacementValidity(gridposition, selectedTowerIndex);
+        previewRenderer.material.color = placementValidity ? Color.green : Color.red;
+
         mouseIndicator.transform.position = mousePosition;
         cellIndicator.transform.position = grid.CellToWorld(gridposition);
     }
