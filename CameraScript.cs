c4e2c2ac6ed52da764b5cacf66f24fa222b1566c
Xing Ye <tanxingye23@gmail.com>
2023-11-11 11:28:16 +0800

    merge conflict dilemma
    Merge conflict broke the game, resorting to placing relevant scripts to be inputted later for non fault merging inquiry

--- CameraScript.cs
+++ CameraScript.cs
@@ -13,8 +13,4 @@ public class CameraScript : MonoBehaviour
         Vector3 finalPosition = player.position + offset;
         transform.position = Vector3.Lerp(transform.position, finalPosition, smoothSpeed * Time.deltaTime);
     }
-<<<<<<< Updated upstream:Assets/Sprites/CameraScript.cs
 }
-=======
-}
->>>>>>> Stashed changes:Assets/Scripts/CameraScript.cs
