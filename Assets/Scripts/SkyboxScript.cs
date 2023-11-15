423c4b2bda595ba84430f0e5e708ac53e7fa6cb6
Alvin <>
2023-11-15 10:42:00 +0800

    Skybox rotation
    Background

---
+++ Assets/Scripts/SkyboxScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxScript : MonoBehaviour
+{
+    public float speed;
+    private Skybox skybox;
+    // Start is called before the first frame update
+    void Start()
+    {
+        skybox = GetComponent<Skybox>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        skybox.material.SetFloat("_Rotation", Time.time * speed);
+    }
+}
